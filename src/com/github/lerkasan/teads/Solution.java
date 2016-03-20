92d470afbd6c57f8ae21b328a26f5de0c7cb2534
Valeriia Lapytska <lerkasan@gmail.com>
2016-03-20 22:57:47 +0200

    moved all Solution class fields and methods to Tree class

--- src/com/github/lerkasan/teads/Solution.java
+++ src/com/github/lerkasan/teads/Solution.java
@@ -1,83 +1,15 @@
 package com.github.lerkasan.teads;
 
-import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.Scanner;
-
 public class Solution {
-	Node root;
-	NumberNodes leaves;
-	NumberNodes allNodes;
-	Connections nodeConnections;
 	
-	public Solution() {
-		root = new Node();
-		leaves = new NumberNodes();
-		allNodes = new NumberNodes();
-		nodeConnections = new Connections();
+	private Solution() {
 	}
 	
-	public Node getRoot() {
-		return root;
-	}
-
-	public NumberNodes getLeaves() {
-		return leaves;
-	}
-
-	public NumberNodes getAllNodes() {
-		return allNodes;
-	}
-
-	public Connections getNodeConnections() {
-		return nodeConnections;
-	}
-
-	public void init(String filePath) { 
-       // try (Scanner in = new Scanner(System.in)) {
-		try (Scanner in = new Scanner(Paths.get(filePath))) {
-        	int n = in.nextInt(); // the number of adjacency relations
-        	for (int i = 0; i < n; i++) {
-        		int fromNumber = in.nextInt(); 
-        		int toNumber = in.nextInt(); 
-        		if (allNodes.add(fromNumber)) {
-        			leaves.add(fromNumber);
-        		} else {
-        			leaves.remove(fromNumber);
-        		} 
-        		if (allNodes.add(toNumber)) {
-        			leaves.add(toNumber);
-        		} else {
-        			leaves.remove(toNumber);
-        		}
-        		if (nodeConnections.containsKey(fromNumber)) {
-        			nodeConnections.add(fromNumber, toNumber);
-        		} else if (nodeConnections.containsKey(toNumber)) {
-        			nodeConnections.add(toNumber, fromNumber);
-        		} else {
-        			nodeConnections.add(fromNumber, toNumber);
-        		}
-        	}
-        }
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void printLeaves() {
-		System.out.println("Leaves: ");
-		for (Integer i : leaves.getNodeSet()) {
-			System.out.print(i+" ");
-		}
-	}
-	
-	public static void main(String[] args) {
-		Solution test1 = new Solution();
-		test1.init("input\\test1.txt");
+	public static void main(String[] args) {	
 		Tree tree1 = new Tree();
-		tree1.build(test1.getNodeConnections());
-		test1.printLeaves();
-
+		tree1.init("input\\test1.txt");
+		tree1.build();
+		tree1.printLeaves();
 	}
 
 }
--- src/com/github/lerkasan/teads/Tree.java
+++ src/com/github/lerkasan/teads/Tree.java
@@ -1,19 +1,80 @@
 package com.github.lerkasan.teads;
 
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Tree {
+	Node root;
+	NumberNodes leaves;
+	NumberNodes allNodes;
+	Connections nodeConnections;
 	
-	public void build(Connections connectedNumbers) {
+	public Tree() {
+		root = new Node();
+		leaves = new NumberNodes();
+		allNodes = new NumberNodes();
+		nodeConnections = new Connections();
+	}
+	
+	public Node getRoot() {
+		return root;
+	}
+
+	public NumberNodes getLeaves() {
+		return leaves;
+	}
+
+	public NumberNodes getAllNodes() {
+		return allNodes;
+	}
+
+	public Connections getNodeConnections() {
+		return nodeConnections;
+	}
+
+	public void init(String filePath) { 
+       // try (Scanner in = new Scanner(System.in)) {
+		try (Scanner in = new Scanner(Paths.get(filePath))) {
+        	int n = in.nextInt(); // the number of adjacency relations
+        	for (int i = 0; i < n; i++) {
+        		int fromNumber = in.nextInt(); 
+        		int toNumber = in.nextInt(); 
+        		if (allNodes.add(fromNumber)) {
+        			leaves.add(fromNumber);
+        		} else {
+        			leaves.remove(fromNumber);
+        		} 
+        		if (allNodes.add(toNumber)) {
+        			leaves.add(toNumber);
+        		} else {
+        			leaves.remove(toNumber);
+        		}
+        		if (nodeConnections.containsKey(fromNumber)) {
+        			nodeConnections.add(fromNumber, toNumber);
+        		} else if (nodeConnections.containsKey(toNumber)) {
+        			nodeConnections.add(toNumber, fromNumber);
+        		} else {
+        			nodeConnections.add(fromNumber, toNumber);
+        		}
+        	}
+        }
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+		
+	public void build() {
 		Node fromNode;
 		Node toNode;
 		Set<Node> possibleRoots = new HashSet<>();
 		Map<Integer, Node> createdNodes = new HashMap<>();
 		Set<Pair<Node>> builtConnections = new HashSet<>();
-		for (Map.Entry<Integer, Set<Integer>> entry : connectedNumbers.getConnectionsMap().entrySet()) {
+		for (Map.Entry<Integer, Set<Integer>> entry : nodeConnections.getConnectionsMap().entrySet()) {
 			Integer key = entry.getKey();
 			if (! createdNodes.containsKey(key)) {
 				fromNode = new Node(key, null);
@@ -58,5 +119,12 @@ public class Tree {
 			}
 			System.out.println();
 		
+	}
+	
+	public void printLeaves() {
+		System.out.println("Leaves: ");
+		for (Integer i : leaves.getNodeSet()) {
+			System.out.print(i+" ");
+		}
 	}
 }
